179219da41afdf2a482017955e877ed1d6e6c054
abilashravi-ta <abilash.ravi@tigeranalytics.com>
2024-03-15 13:52:57 +0530

    plain simple_functions.py

--- simple_functions.py
+++ simple_functions.py
@@ -1,15 +1,7 @@
 # Custom python functions
 
 def double_number(a):
-    """A function to double a given number."""
-    print(f'value before double_number(): {a}')
-    result = a+a
-    print(f'value after double_number(): {result}')
-    return result
+    return a+a
 
 def square_number(a):
-    """A function to square a given number."""
-    print(f'value before square_number(): {a}')
-    result = a*a
-    print(f'value after square_number(): {result}')
-    return result
+    return a*a
