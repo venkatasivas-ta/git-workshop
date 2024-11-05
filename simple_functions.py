a0a024790a6d2712cc4e9f817c3839afb00e3121
Sai Tarun <atlasai.tarunred@tigeranalytics.com>
2024-11-05 06:34:42 +0000

    Function Definition

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,15 @@
 # Custom python functions
 
+
 def double_number(a):
-    return a+a
+    print(f"value before double_number(): {a}")
+    result = double_number(a)
+    print(f"value after double_number(): {result}")
+    return
+
 
 def square_number(a):
-    return a*a
+    print(f"value before square_number(): {a}")
+    result = square_number(a)
+    print(f"value after square_number(): {result}")
+    return
