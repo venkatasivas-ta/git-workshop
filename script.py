320504d2ca2f3adc0fc2856b0f415a646a589b62
jananiharivardhini-ta <janani.harivardh@tigeranalytics.com>
2024-11-05 12:25:28 +0530

    Creating docstrings

--- script.py
+++ script.py
@@ -1,3 +1,14 @@
+"""
+This script demonstrates the use of functions imported from the simple_functions module.
+It includes examples of doubling and squaring a number.
+
+Functions:
+    double_number: Doubles the input number.
+    square_number: Squares the input number.
+
+Usage:
+    Run the script to see the output of the functions with the example value.
+"""
 from simple_functions import double_number, square_number
 
 a = 5
--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,24 @@
 # Custom python functions
-
 def double_number(a):
+    """
+    Doubles the input number.
+
+    Parameters:
+    a (int or float): The number to be doubled.
+
+    Returns:
+    int or float: The doubled value of the input number.
+    """
     return a+a
 
 def square_number(a):
+    """
+    Squares the input number.
+
+    Parameters:
+    a (int or float): The number to be squared.
+
+    Returns:
+    int or float: The squared value of the input number.
+    """
     return a*a
